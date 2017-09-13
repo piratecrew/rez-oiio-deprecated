2e90826502ee09bf5ad57a42cea2c677182e201e
Fredrik Brännbacka FL <fredrik.brannbacka@ilpvfx.com>
2017-09-13 16:58:31 +0200

    full path to library

--- cmake/oiio.cmake
+++ cmake/oiio.cmake
@@ -1,3 +1,3 @@
 set(oiio_INCLUDE_DIRS   	${REZ_OIIO_ROOT}/include)
 set(oiio_LIBRARY_DIRS   	${REZ_OIIO_ROOT}/lib)
-set(oiio_LIBRARIES 	      OpenImageIO)
+set(oiio_LIBRARIES 	      ${oiio_LIBRARY_DIRS}/libOpenImageIO.so)
